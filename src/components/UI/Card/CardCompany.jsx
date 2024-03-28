eb26e75cbf3088b20cb609e1b54736091d397533
Stevie Rossetti <rossetti.stephen@gmail.com>
2024-03-28 13:41:47 -0500

    Template code from Activity 21 React Router

---
+++ src/components/UI/Card/CardCompany.jsx
@@ -0,0 +1,9 @@
+export default function CardCompany({ name, catchPhrase }) {
+    return (
+      <section className="ms-2 me-auto">
+        <h4>{name}:</h4>
+        <h5 className="card-text">{catchPhrase}</h5>
+      </section>
+    );
+  }
+  
