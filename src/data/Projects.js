6e46283cdf7a4b13151ffb6aec84125487014ca2
Stevie Rossetti <rossetti.stephen@gmail.com>
2024-03-31 22:32:09 -0500

    Add date for projects page. Also create portfolio page to map

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,17 +1,60 @@
 const projectsData =
 [
     {
+      "id": 1,
       "title": "Model-View-Controller (MVC): Tech-Blog",
       "content": "CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers posts as well.",
       "gitHubUrl": "https://github.com/stephenrossetti/Rossetti-s-Model-View-Controller-MVC-Tech-Blog",
-      "liveUrl": "https://rossetti-mvc-tech-blog-5f23ac01a35f.herokuapp.com/"
+      "liveUrl": "https://rossetti-mvc-tech-blog-5f23ac01a35f.herokuapp.com/",
+      "image": "../src/assets/images/p1TechBlog.png"
     },
     {
+      "id": 2,
       "title": "Progressive Web Applications (PWA): Text Editor",
-      "content": "Aa text editor that runs in the browser and is a single-page application that meets the PWA criteria. ",
+      "content": "A text editor that runs in the browser and is a single-page application that meets the PWA criteria.",
       "gitHubUrl": "https://github.com/stephenrossetti/Rossetti-s-Progressive-Web-Applications-PWA-Challenge-Text-Editor",
-      "liveUrl": "https://rossettis-progressive-web-applications.onrender.com/"
-
+      "liveUrl": "https://rossettis-progressive-web-applications.onrender.com/",
+      "image": "../src/assets/images/p2PWA.png"
+    },
+    {
+      "id": 3,
+      "title": "SQL: Employee Tracker",
+      "content": "A command-line application to manage a company's employee database and act as a content management system (CMS).",
+      "gitHubUrl": "https://github.com/stephenrossetti/SQL-Challenge-Employee-Tracker",
+      "liveUrl": "https://drive.google.com/file/d/1UKzp6wDt7Con_gm9E5rFRI5ihtZNHamA/view",
+      "image": "../src/assets/images/p3EmployeeTracker.png"
+    },
+    {
+      "id": 4,
+      "title": "Express.js Note Tracker",
+      "content": "A application via Express.js (backend) that can be used to write, save and delete notes.",
+      "gitHubUrl": "https://github.com/stephenrossetti/Express.Js-Note-Tracker",
+      "liveUrl": "https://express-js-note-tracker-bfbfe455c9cc.herokuapp.com/",
+      "image": "../src/assets/images/p4NoteTracker.png"
+    },
+    {
+      "id": 5,
+      "title": "Server-Side APIs: Weather Dashboard",
+      "content": "A 5-day weather dashboard that accesses a third-party's API (OpenWeatherMap) and retrieves data from their application.",
+      "gitHubUrl": "https://github.com/stephenrossetti/Module-6-Challenge",
+      "liveUrl": "https://stephenrossetti.github.io/Module-6-Challenge/",
+      "image": "../src/assets/images/p5WeatherApp.png"
+    },
+    {
+      "id": 6,
+      "title": "Pet Treat Informer",
+      "content": "A website where dog owners can search common human foods they want to feed their dogs and obtain nutritional information on those foods.",
+      "gitHubUrl": "https://github.com/stephenrossetti/Project-1",
+      "liveUrl": "https://stephenrossetti.github.io/Project-1/",
+      "image": "../src/assets/images/p6PetTreatInformer.png"
+    },
+    {
+      "id": 7,
+      "title": "Convention Planner",
+      "content": "A web-application that will allows users to create and save new events at convention and when logged in they can view all events that have been created.",
+      "gitHubUrl": "https://github.com/kathylopez97/Convention-Planner",
+      "liveUrl": "https://convention-planner-f58cd02bb10c.herokuapp.com/",
+      "image": "../src/assets/images/p7ConventionPlanner.png"
     }
   ]
 
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,38 +1,36 @@
-import React from "react";
+import React from 'react';
+import Projects from '../data/Projects'; // Import JSON data
 
-// This is a static page mocking an "About Us" section
-export default function AboutPage() {
+export default function Portfolio() {
   return (
-    <section id="about-me" className="my-5 intro">
-      <div className="flex-row">
-        <h1 id="about" className="section-title">
-          About Me
-        </h1>
-      </div>
-      <div className="flex-row">
-        <div className="intro-info">
-          <p style={{ fontWeight: 'bold' }}>
-            Professional
-          </p>
-          <p>
-            I am a Certified Public Accountant (CPA) and I graduated from the University of Massachusetts, Amherst - Isenberg
-            School of Management with a Bachelor of Business Administration (BBA) focused in Accounting. After graduation, I
-            worked at Ernst & Young from September 2016 to October 2017 as an assurance auditor for software, manufacturing,
-            pharmaceutical, and biomedical clients throughout the Northeast. In October 2017, I moved to Minnesota and received my official
-            CPA license in January 2018. I then began working at Regis Corporation as a Senior SEC Accountant from January 2018 to May 2019.
-            In May 2019, I began working at Target Corporation as an SEC Accountant and subsequently as a Senior Equity Accountant and 
-            Senior Vendor Income Accountant. I am currently still working at Target Corporation as a Vendor Income Accounting Manager.
-          </p>
-          <p style={{ fontWeight: 'bold' }}>
-            Personal
-          </p>
-          <p>
-            I am married to my lovely wife, Christina, who works as an exotics veterinarian. We have three pets named Layla (bird),
-            Josie (dog), and Axel (cat). Some of my hobbies include hiking, football, video games, and learning to code! Speaking of hiking,
-            I recently completed the Appalachian Trail, which is a  2,200 mile, 120 day trek from George to Maine.
-          </p>
+    <div>
+      <section id="work" className="projects">
+        <div className="flex-row">
+          <h2 className="section-title">Projects</h2>
         </div>
-      </div>
-    </section>
+
+        {Projects.map((project) => (
+          <div className="project" key={project.id}>
+            <div className="project-info">
+              <div className="project-img">
+                <a href={project.gitHubUrl} target="_blank" rel="noopener noreferrer">
+                  <img
+                    src={project.image}
+                    className="my-2"
+                    style={{ width: '100%' }}
+                    alt={project.title}
+                  />
+                </a>
+              </div>
+              <div className="project-text">
+                <h4>{project.title}</h4>
+                <p>{project.content}</p>
+                <a href={project.liveUrl} target="_blank" rel="noopener noreferrer">Live Demo</a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
+    </div>
   );
 }
