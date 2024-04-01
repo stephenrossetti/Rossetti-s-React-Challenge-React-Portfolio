fcefc752f05dc84f0750acfe346e540f47e37109
Stevie Rossetti <rossetti.stephen@gmail.com>
2024-03-31 21:37:02 -0500

    Debugging the boolean for NavLink

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,19 +7,19 @@ export default function Nav() {
   return (
     <Navbar
       links={[
-        <NavLink key={1} className="nav-link text-light" activeClassName="active" exact to="/">
+        <NavLink key={1} className="nav-link text-light" activeclassname="active" exact="true"  to="/">
           Home
         </NavLink>,
-        <NavLink key={2} className="nav-link text-light" activeClassName="active" to="/about">
+        <NavLink key={2} className="nav-link text-light" activeclassname="active" to="/about">
           About Me
         </NavLink>,
-        <NavLink key={3} className="nav-link text-light" activeClassName="active" to="/portfolio">
+        <NavLink key={3} className="nav-link text-light" activeclassname="active" to="/portfolio">
           Portfolio
         </NavLink>,
-        <NavLink key={4} className="nav-link text-light" activeClassName="active" to="/contact">
+        <NavLink key={4} className="nav-link text-light" activeclassname="active" to="/contact">
           Contact
         </NavLink>,
-        <NavLink key={5} className="nav-link text-light" activeClassName="active" to="/resume">
+        <NavLink key={5} className="nav-link text-light" activeclassname="active" to="/resume">
           Resume
         </NavLink>,
       ]}
